bb1aaadd456c0f87d0a0748b343bc29497d214d8
Amidu99 <amidushamika@gmail.com>
2024-06-18 22:14:16 +0530

    add EmployeeService

--- Mongo_BE/src/main/java/lk/ijse/mongo_be/service/EmployeeService.java
+++ Mongo_BE/src/main/java/lk/ijse/mongo_be/service/EmployeeService.java
@@ -4,5 +4,11 @@ import lk.ijse.mongo_be.dto.EmployeeDTO;
 import java.util.List;
 
 public interface EmployeeService {
-
+    void saveEmployee(EmployeeDTO employeeDTO);
+    boolean existsByEmployeeCode(String employeeCode);
+    EmployeeDTO getEmployeeByEmployeeCode(String employeeCode);
+    List<EmployeeDTO> getAllEmployees();
+    String getLastEmployeeCode();
+    void updateEmployee(EmployeeDTO employeeDTO);
+    void deleteEmployee(String employeeCode);
 }
