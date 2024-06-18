c91921ad3b53ef05858a56082ecbb03568460238
Amidu99 <amidushamika@gmail.com>
2024-06-18 22:08:56 +0530

    add EmployeeServiceIMPL

---
+++ Mongo_BE/src/main/java/lk/ijse/mongo_be/service/impl/EmployeeServiceIMPL.java
@@ -0,0 +1,16 @@
+package lk.ijse.mongo_be.service.impl;
+
+import lk.ijse.mongo_be.document.Employee;
+import lk.ijse.mongo_be.dto.EmployeeDTO;
+import lk.ijse.mongo_be.repo.EmployeeRepo;
+import lk.ijse.mongo_be.service.EmployeeService;
+import lk.ijse.mongo_be.util.Mapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceIMPL implements EmployeeService {
+
+}
