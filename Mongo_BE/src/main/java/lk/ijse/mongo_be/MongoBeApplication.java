ddf79e1e371f6d0e95918d1038918a15d8045bc3
Amidu99 <amidushamika@gmail.com>
2024-06-18 20:59:47 +0530

    add Mongo_BE

---
+++ Mongo_BE/src/main/java/lk/ijse/mongo_be/MongoBeApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.mongo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoBeApplication.class, args);
+    }
+
+}
---
+++ Mongo_BE/src/test/java/lk/ijse/mongo_be/MongoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.mongo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
