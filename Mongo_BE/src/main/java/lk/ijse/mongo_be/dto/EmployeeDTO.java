fd058b54c6b7ca3a5c6b831bf02836a359a09645
Amidu99 <amidushamika@gmail.com>
2024-06-18 22:05:07 +0530

    add EmployeeDTO

---
+++ Mongo_BE/src/main/java/lk/ijse/mongo_be/dto/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.mongo_be.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EmployeeDTO implements SuperDTO {
+    private String employeeCode;
+    private String employeeName;
+    private String employeeEmail;
+    private String employeePic;
+}
