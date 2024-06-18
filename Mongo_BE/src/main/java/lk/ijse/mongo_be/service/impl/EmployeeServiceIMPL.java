eb0b525d0ccbf1678fd500fdb0731c6700c128c7
Amidu99 <amidushamika@gmail.com>
2024-06-18 22:14:38 +0530

    add EmployeeServiceIMPL

--- Mongo_BE/src/main/java/lk/ijse/mongo_be/service/impl/EmployeeServiceIMPL.java
+++ Mongo_BE/src/main/java/lk/ijse/mongo_be/service/impl/EmployeeServiceIMPL.java
@@ -12,5 +12,46 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceIMPL implements EmployeeService {
+    private final EmployeeRepo repo;
+    private final Mapping mapping;
 
+    @Override
+    public void saveEmployee(EmployeeDTO employeeDTO) {
+        repo.save(mapping.toEmployeeEntity(employeeDTO));
+    }
+
+    @Override
+    public boolean existsByEmployeeCode(String employeeCode) {
+        return repo.existsByEmployeeCode(employeeCode);
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeByEmployeeCode(String employeeCode) {
+        return mapping.toEmployeeDTO(repo.getEmployeeByEmployeeCode(employeeCode));
+    }
+
+    @Override
+    public List<EmployeeDTO> getAllEmployees() {
+        return mapping.toEmployeeDTOList(repo.findAll());
+    }
+
+    @Override
+    public String getLastEmployeeCode() {
+        Employee employee = repo.findTopByOrderByEmployeeCodeDesc();
+        return employee != null ? employee.getEmployeeCode() : null;
+    }
+
+    @Override
+    public void updateEmployee(EmployeeDTO employeeDTO) {
+        Employee existingEmployee = repo.getEmployeeByEmployeeCode(employeeDTO.getEmployeeCode());
+        existingEmployee.setEmployeeName(employeeDTO.getEmployeeName());
+        existingEmployee.setEmployeeEmail(employeeDTO.getEmployeeEmail());
+        existingEmployee.setEmployeePic(employeeDTO.getEmployeePic());
+        repo.save(existingEmployee);
+    }
+
+    @Override
+    public void deleteEmployee(String employeeCode) {
+        repo.deleteById(employeeCode);
+    }
 }
